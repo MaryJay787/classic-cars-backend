3b80eb94af6db18a70d18ed635969971bdcfc25f
MaryJay787 <josephmaryam.mj@gmail.com>
2019-08-19 16:55:19 -0400

    login successfull

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -17,6 +17,6 @@ class AuthenticationController < ApplicationController
  
   def user_login_params
     # params { user: {username: 'Chandler Bing', password: 'hi' } }
-    params.permit(:username, :password, :image)
+    params.permit(:username, :password)
   end
 end
