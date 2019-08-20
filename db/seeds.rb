03f225dd6a2c1170c8e5452c9efce4fc20e531d9
Aeisha Hicks <ashicks2017@outlook.com>
2019-08-20 12:34:31 -0400

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ interior: ' Black leather interior', user_id: user1.id)
 car2 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6666/415294/790x1024/1969-chevrolet-camaro', 
     year:'1969', make:'Chevrolet', model: 'Camaro', 
     engine: '632 V8', trans: 'Manual',  
-    body_type:'', drive:'5 SPEED ', 
+    body_type:'Sport', drive:'5 SPEED ', 
     color: 'HUGGER ORANGE / Pearl White Rally Stripes' , interior_color: 'BLACK', 
     interior: 'Customized leather interior', user_id: user1.id)
 
@@ -70,7 +70,7 @@ car8 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/4687/399072/790
 car9 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6921/434891/790x1024/1987-buick-regal-gnx', 
         year:'1987', make:'Buick', model: 'Regal GNX', 
         engine: '3.8L V6 TURBO', trans: 'AUTOMATIC',  
-        body_type:' ', drive:'4 SPEED  ', 
+        body_type:'Regal ', drive:'4 SPEED  ', 
         color: 'BLACK ' , interior_color: 'GRAY', 
         interior: 'Original tri-tone interior ', user_id: user1.id)
 
@@ -125,6 +125,117 @@ car16 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6750/422599/79
         body_type:'Camaro Convertible', drive:'4 SPEED', 
         color: 'ORANGE ' , interior_color: 'BLACK ', 
         interior: 'Deluxe Interior ', user_id: user3.id)
+
+car16 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6750/422599/790x1024/1969-chevrolet-camaro', 
+        year:'1969', make:'Chevrolet', model: 'Camaro', 
+        engine: '396 V8 ', trans: 'MANUAL',  
+        body_type:'Camaro Convertible', drive:'4 SPEED', 
+        color: 'ORANGE ' , interior_color: 'BLACK ', 
+        interior: 'Deluxe Interior ', user_id: user3.id)
+
+car17 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/7004/441888/790x1024/1965-chevrolet-c10-pickup-truck', 
+        year:'1965', make:'Chevrolet', model: 'C10 Pickup Truck', 
+        engine: '350 V8 ', trans: 'AUTOMATIC',  
+        body_type:'Truck', drive:'3 SPEED', 
+        color: 'VIPER RED' , interior_color: 'TAN', 
+        interior: 'Customized leather interior ', user_id: user3.id)
+
+
+car18 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6867/432399/790x1024/1969-chevrolet-brookwood-station-wagon', 
+        year:'1969', make:'Chevrolet', model: 'Brookwood Station Wagon', 
+        engine: 'SUPERCHARGED 6.2L LS3 V8', trans: 'AUTOMATIC',  
+        body_type:'Station Wagon', drive:'4 SPEED', 
+        color: 'RALLY RED' , interior_color: 'BLACK', 
+        interior: 'Custom interior ', user_id: user3.id)
+
+car19 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/5351/400091/790x1024/1969-ford-mustang-boss-429', 
+        year:'1969', make:'Ford', model: 'Mustang Boss 429', 
+        engine: '429 V8', trans: 'MANUAL',  
+        body_type:'Sport', drive:'4 SPEED', 
+        color: 'WIMBLEDON WHITE' , interior_color: 'BLACK', 
+        interior: ' Correct Black Clarion Knit vinyl interior', user_id: user3.id)
+
+car20 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6817/424263/790x1024/1969-oldsmobile-toronado', 
+        year:'1969', make:'Oldsmobile', model: 'Toronado', 
+        engine: '455 V8', trans: 'MANUAL',  
+        body_type:'Classic Oldsmobile', drive:'3 SPEED', 
+        color: 'MEADOW GREEN METALLIC' , interior_color: 'GREEN', 
+        interior: 'Largely-original cloth and vinyl interio ', user_id: user3.id)
+
+car21 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6625/419648/790x1024/1969-oldsmobile-cutlass-442', 
+        year:'1969', make:' Oldsmobile', model: 'Cutlass 442', 
+        engine: '400 V8', trans: 'AUTOMATIC',  
+        body_type:'Convertible ', drive:'3 SPEED ', 
+        color: 'AZTEC GOLD' , interior_color: 'SADDLE', 
+        interior: ' ', user_id: user3.id)
+
+
+car22 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6913/433179/790x1024/1940-willys-americar-coupe', 
+        year:'1940', make:'Willys', model: 'Americar Coupe', 
+        engine: '383 V8', trans: 'AUTOMATIC',  
+        body_type:'Streetrod ', drive:'4 SPEED', 
+        color: 'ORANGE PEARL METALLIC' , interior_color: 'TAN', 
+        interior: ' Custom leather interior', user_id: user2.id)
+
+
+car23 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6957/438467/790x1024/1952-chevrolet-3100-pickup-truck', 
+        year:'1952', make:'Chevrolet ', model: '3100 Pickup Truck', 
+        engine: '6.0L VORTEC V8', trans: 'AUTOMATIC',  
+        body_type:'Truck', drive:'4 SPEED ', 
+        color: 'White' , interior_color: 'TERRACOTTA', 
+        interior: 'ustom Terracotta leather interior ', user_id: user2.id)
+
+
+car24 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/5366/384860/790x1024/1973-porsche-911-s', 
+        year:'1973', make:'Porsche ', model: '911 S', 
+        engine: '2.4L SIX CYLINDER', trans: 'MANUAL',  
+        body_type:'Sport', drive:'5 SPEED', 
+        color: 'SILVER METALLIC' , interior_color: 'BLACK', 
+        interior: 'Correct Black vinyl interior ', user_id: user2.id)
+
+car25 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6967/440627/790x1024/1970-mercury-cougar-eliminator', 
+        year:'1970', make:' Mercury', model: 'Cougar Eliminator', 
+        engine: '428 COBRA JET V8', trans: 'MANUAL',  
+        body_type:'', drive:'4 SPEED', 
+        color: 'COMPETITION BLUE' , interior_color: 'WHITE', 
+        interior: 'White Clarion Knit Décor interior  ', user_id: user2.id)
+
+
+car26 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6593/416886/790x1024/1978-pontiac-firebird-trans-am', 
+        year:'1978', make:'Pontiac ', model: 'Firebird Trans Am', 
+        engine: '5.3L GM VORTEC V8', trans: ' AUTOMATIC',  
+        body_type:'', drive:'4 SPEED', 
+        color: 'PEWTER SILVER' , interior_color: 'GRAY', 
+        interior: 'Custom interior', user_id: user2.id)
+
+car27 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6909/437620/790x1024/1978-toyota-land-cruiser-fj43', 
+        year:'1978 ', make:'Toyota', model: 'Land Cruiser FJ43', 
+        engine: 'Correct 2F 6-cylinder', trans: 'MANUAL',  
+        body_type:'Jeep', drive:'5 Speed', 
+        color: 'DUNE BEIGE' , interior_color: 'GRAY', 
+        interior: 'Vinyl split-bench interior', user_id: user2.id)
+
+car28 =  Car.create(image:'https://s8096.pcdn.co/wp-content/uploads/2015/02/1958-1960-Ford-Thunderbird.jpg', 
+        year:'1958', make:'Ford ', model: 'Thunderbird', 
+        engine: '5.8 liter V8 engine', trans: 'MANUAL',  
+        body_type:'Convertible', drive:'4 Speed', 
+        color: 'Pascal green' , interior_color: 'Custom Pascal green', 
+        interior: 'Vinyl interior ', user_id: user2.id)
+
+
+car29 =  Car.create(image:'https://www.oldcartours.com/media/galleries/photos/2017/07/29/chevrolet-50-pink-convertible-1.jpg', 
+        year:'1950', make:'Chevrolet ', model: 'Chevrolet Deluxe', 
+        engine: '6 cylinders in-line', trans: 'AUTOMATIC',  
+        body_type:'Convertible', drive:'3 Speed', 
+        color: 'Hot Pink' , interior_color: 'White', 
+        interior: 'Custom interior', user_id: user2.id)
+
+car30 =  Car.create(image:'https://cdn.dealeraccelerate.com/rkm/1/6916/432239/790x1024/1989-ferrari-testarossa', 
+        year:'1989', make:'Ferrari', model: 'Testarossa', 
+        engine: '4.9L 12 CYLINDER', trans: 'MANUAL',  
+        body_type:'Sport', drive:'5 SPEED', 
+        color: 'ROSSO CORSA' , interior_color: 'TAN', 
+        interior: 'Leather interior', user_id: user2.id)
 
 
 
